6bce18fbfb20406e3f49a681be6a56c15082a017
Vincent Sijben <vincent.sijben@zuyd.nl>
2022-06-08 16:00:42 +0200

    fix2

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # touchdesigner-websocket
 
 Open `touchdesigner.xd` and install plugin "Web Export" from Velara 3.
-When installed, select any element and choose **Plugins -> Web Export -> Element** options.
+When installed, select any element and choose **Plugins -> Web Export -> Element options**.
 
-Add the class "touchdesigner" to any element you want to click and send the websocket message from.
+Add the class `touchdesigner` to any element you want to click and send the websocket message from.
 In the attributes field, add `data-touchdesigner="mydata"`. The string "mydata" will be sent if you click the button in the final result. You can check the blue button to see some added element options.
+If you omit this data-touchdesigner, the generated id from the xd component will be sent.
 
 When you're done selecting and adding meta data to your elements, go to **Plugins - Web Export - Export artboard**. The `index.html` you'll find in this repo is an example export.
 
--- touchdesigner.js
+++ touchdesigner.js
@@ -1,18 +1,13 @@
 
-	(() => {
-             // Initialize a new TCP connection on port 9980
-		const socket = new WebSocket("ws://localhost:9980")
-		let buttons = document.querySelectorAll(".touchdesigner");
-        buttons.forEach(e=>{
-            e.addEventListener("click", function(event){
-                console.log('clicked')
-                socket.send(`${e.getAttribute("id")} was clicked`)
-            });
-        })
-        // const wipe = document.querySelector("#wipe")
-              
-		// wipe.onclick = () =>
-        //          // Send a control message!
-        //         //  socket.send(JSON.stringify({ type: "ctrl:wipe" }))
-        //          socket.send("hallo, er is geklikt op een button")
-	})()
+(() => {
+    let port = 9980;
+    const socket = new WebSocket(`ws://localhost:${port}`)
+    let buttons = document.querySelectorAll(".touchdesigner");
+    buttons.forEach(e => {
+        e.addEventListener("click", function (event) {
+            let data_touchdesigner = e.getAttribute("data-touchdesigner");
+            if (data_touchdesigner === null) data_touchdesigner = e.getAttribute("id");
+            socket.send(data_touchdesigner);
+        });
+    })
+})()
